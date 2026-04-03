53a65b0c9234093ab2a6bf3f13e8c25efb517ae9
Mark Jericho Manilag <jerichodon1998@gmail.com>
2026-04-03 13:41:11 +0800

    chore: implement broadcast

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { configDotenv } from "dotenv";
 
 import FastifyWebSocket from "@fastify/websocket";
-import Fastify from "fastify";
+import Fastify, { FastifyRequest } from "fastify";
+import { WebSocket } from "ws";
 import cors from "@fastify/cors";
 
 configDotenv();
+
+const clients: Map<string, WebSocket> = new Map();
 
 const fastify = Fastify({
 	logger: {
@@ -26,25 +29,57 @@ fastify.register(cors, {
 	},
 });
 
+function broadcast(params: { message: string }) {
+	for (const [_, socket] of clients) {
+		if (socket.readyState === WebSocket.OPEN) {
+			console.log("broadcasting", params.message);
+			socket.send(params.message);
+		}
+	}
+}
+
 fastify.register(async function (fastify) {
-	fastify.get("/ws", { websocket: true }, async (socket, request: any) => {
-		console.log("user", request?.user?.id);
-		socket.on("message", (message) => {
-			let parsedMessage = "";
-			try {
-				parsedMessage = message.toString();
-				console.log("parsedMessage", parsedMessage);
-			} catch (error) {
-				console.log("error", error);
+	fastify.get(
+		"/ws",
+		{ websocket: true },
+		async (
+			socket,
+			request: FastifyRequest<{
+				Querystring: {
+					userId: string;
+				};
+			}>
+		) => {
+			const userId = request?.query?.userId;
+
+			if (!userId) {
+				socket.close(1000, "No userId provided");
+				return;
 			}
 
-			socket.send(parsedMessage);
-		});
+			clients.set(userId, socket);
+			console.log("clients", clients.keys());
 
-		socket.on("close", () => {
-			console.log("user disconnected");
-		});
-	});
+			socket.on("message", (message: Buffer | ArrayBuffer | Buffer[]) => {
+				let parsedMessage = "";
+
+				try {
+					parsedMessage = JSON.parse(message?.toString());
+					console.log("parsedMessage", parsedMessage);
+				} catch (error) {
+					console.log("error", error);
+				}
+
+				broadcast({ message: JSON.stringify(parsedMessage) });
+			});
+
+			socket.on("close", () => {
+				console.log("user disconnected");
+				clients.delete(userId);
+				console.log("clients", clients.keys());
+			});
+		}
+	);
 });
 
 fastify.get("/", (request, reply) => {
